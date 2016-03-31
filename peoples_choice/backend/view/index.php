ea2295e6cfea3ef14738efc4fda859dca14d7828
Wes Kelly <weskelly@mac.com>
2016-03-31 18:23:06 -0400

    Add sha256 code for password hashing.
    Add some test code to index.php to verify sha256 is working as intended.

--- peoples_choice/backend/view/index.php
+++ peoples_choice/backend/view/index.php
@@ -1,1 +1,14 @@
-<?php echo 'this is a test'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="../model/js/sha256.js"></script>
+	<script>
+		document.addEventListener("DOMContentLoaded", function () {
+			document.getElementById('output').innerHTML = Sha256.hash('abc');
+		});
+	</script>
+</head>
+<body>
+<p id="output"></p>
+</body>
+</html>
