5cbebae8af9ee60cf74650db5a91d5ecb0ff18ac
Joel Sabol <jsabol@cedarville.edu>
2016-04-01 23:25:49 -0400

    Updated the team model

--- peoples_choice/backend/model/team.php
+++ peoples_choice/backend/model/team.php
@@ -1,29 +1,7 @@
 <?php 
-	class Team extends Model{
-		private $project;
-		private $members;
-		
-		public static function project($project){
-			$team = new Team();
-			$team->project = $project;
-			return $team;
-		}
+	class TeamModel extends Model{
 
-		public static function members($members){
-			$team = new Team();
-			$team->members = $members;
-			return $team;
-		}
-
-		public static function fullTeam($project, $members)
-		{
-			$team = new Team();
-			$team->project = $project;
-			$team->members = $members;
-			return $team;
-		}
-
-		public function createInDB()
+		public function create($team)
 		{		
 			$this->beginTransaction();
 
@@ -34,16 +12,30 @@
 			$miplid = mysqli_fetch_assoc($result)['miplid'] + 1;
 			$this->queryInTransaction("insert into implementation values($miplid)");
 
+			$project = $team->project;
 			# Add the users to the implementation
 			$query = 'insert into team values';
-			foreach ($this->members as $member) {
-				$query .= "('$member', '$this->project', $miplid),";
+			foreach ($team->members as $member) {
+				$query .= "('$member', '$project', $miplid),";
 			}
 			$query = rtrim($query, ",");
+
 			$this->queryInTransaction($query);
 
 			# End the transaction
 			return $this->endTransaction();
+		}
+
+		public function destroy($id)
+		{
+			$this->beginTransaction();
+			$this->queryInTransaction("delete from team where implementationID=$id");
+			$this->queryInTransaction("delete from implementation where implementationID=$id");
+			$this->queryInTransaction("set foreign_key_checks=0");
+			$this->queryInTransaction("update implementation set implementationID = implementationID-1 where implementationID > $id");
+			$this->queryInTransaction("update team set implementationID=implementationID-1 where implementationID > $id");
+			$this->queryInTransaction("set foreign_key_checks=1");
+			$this->endTransaction();
 		}
 
 		public function getTeamsForProject($project)
@@ -55,12 +47,12 @@
 			while($row = mysqli_fetch_assoc($result)){
 				$id = $row['implementationID'];
 				$teamQuery = $this->queryInTransaction("select realName from team t, student s where t.username = s.username and implementationID=$id");
-				$team = [];
+				$members = [];
 				while($r = mysqli_fetch_assoc($teamQuery)){
-					array_push($team, $r['realName']);
+					array_push($members, $r['realName']);
 				}
-				array_push($teams, $team);
-				unset($team);
+				array_push($teams, Team::fullTeam($id, $members, $project));
+				unset($members);
 			}
 			
 			$this->endTransaction();
@@ -72,5 +64,33 @@
 		{
 			# code...
 		}
+	}
+
+	/**
+	* 
+	*/
+	class Team
+	{
+		public $project;
+		public $teamid;
+		public $members;
+
+		public static function membersProject($project, $members)
+		{
+			$r = new Team();
+			$r->project = $project;
+			$r->members = $members;
+			return $r;	
+		}
+
+		public function fullTeam($teamid, $project, $members)
+		{
+			$t = new Team();
+			$t->$teamid = $teamid;
+			$t->project = $project;
+			$t->members = $members;
+			return $t;
+		}
+		
 	}
  ?>
