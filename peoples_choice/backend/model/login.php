005e9838031ce1f56496d37b93a61fafc6c62fb5
Joel Sabol <jsabol@cedarville.edu>
2016-04-04 10:29:04 -0400

    Updated admin content so it can only be viewed by administrators

--- peoples_choice/backend/model/login.php
+++ peoples_choice/backend/model/login.php
@@ -9,7 +9,8 @@
 			$user = $_POST['username'];
 			$password = $_POST['password'];
 
-			$result = mysqli_query($this->connection, "select * from student where username='$user'");
+			//$result = mysqli_query($this->connection, "select * from student where username='$user'");
+			$result = mysqli_query($this->connection, "select s.username, realName, pwHash, coalesce(a.username, 'false') isAdmin from student s left join admin a on s.username=a.username where s.username='$user'");
 
 			$loginStatus = array('login' => false);
 
@@ -18,7 +19,8 @@
 				if($row['username'] == $user && $row['pwHash'] == $password){
 					$_SESSION['logged-in'] = true;
 					$_SESSION['username'] = $row['realName'];
-					$loginStatus = array('login'=>true, 'user'=>$row['realName']);
+					$_SESSION['isAdmin'] = $row['isAdmin'] == 'false' ? false:true;
+					$loginStatus = array('login'=>true, 'user'=>$row['realName'], 'isAdmin'=>$_SESSION['isAdmin']);
 				}
 			}
 			echo json_encode($loginStatus);
