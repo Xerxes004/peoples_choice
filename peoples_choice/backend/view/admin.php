8b80f9d5edb5f3a56e3350f24885ae384a87a014
Wes Kelly <weskelly@mac.com>
2016-04-04 12:31:02 -0400

    Add basic functionality for team-builiding to Admin view.

--- peoples_choice/backend/view/admin.php
+++ peoples_choice/backend/view/admin.php
@@ -3,64 +3,65 @@
 		<ul class="nav nav-tabs">
 			<li class="active"><a data-toggle="tab" href="#user-admin">User Administration</a></li>
 			<li><a data-toggle="tab" href="#project-admin">Project Administration</a></li>
+			<li><a data-toggle="tab" href="#team-admin">Team Administration</a></li>
 		</ul>
 	</div>
 		<div class="tab-content panel-body">
 			<div id="user-admin" class="tab-pane fade in active">
-					<h2>User Administration</h2>
-					<div class="form-group">
-					  <label for="user-select">Select User:</label>
-					  <select class="form-control" id="user-select">
-					  	<option></option>
-					    <?php 
-					    	foreach ($data['students'] as $key => $student) {
-					    		$realName = $student->realName;
-					    		echo "<option value='$key'>$realName<option>";
-					    	}
-					     ?>
-					  </select>
-					</div>
-					<div class="well">
-						<div id="user-updated" class="alert alert-success hide">
-						  <a href="#" class="close" data-dismiss="alert" aria-label="close"></a>
-				     	  <div id="update-msg"></div>
-					    </div>
-						<div class="form-group">
-						  <label for="new-name">Change Full Name:</label>
-						  <input type="text" class="form-control" id="new-name">
-						</div>
-						<div class="form-group">
-						  <label for="new-linux">Change Linux Username:</label>
-						  <input type="text" class="form-control" id="new-linux">
-						</div>
-						<div class="well checkbox">
-							<label><input id="update-admin-checkbox" type="checkbox" value="admin">Admin</label>
-						</div>
-						<button style="margin-left: 20px;" onclick="resetPassword(event)">Reset Password</button>
-						<button onclick="deleteUser(event)">Delete User</button>
-						<button onclick="updateUser(event)">Update</button>
-					</div>
-
-					<h2>User Add</h2>
-					<div class="well">
-						<div id="user-added" class="alert alert-success hide">
-						  <a href="#" class="close" data-dismiss="alert" aria-label="close"></a>
-				     	  <div id="msg"></div>
-					    </div>
-						<div class="form-group">
-						  <label for="usr">Name:</label>
-						  <input type="text" class="form-control" id="add-user">
-						</div>
-						<div class="form-group">
-						  <label for="usr">Linux Username:</label>
-						  <input type="text" class="form-control" id="add-linux">
-						</div>
-						<div class="well checkbox">
-								<label><input id="add-admin-checkbox" type="checkbox" value="admin">Admin</label>
-							</div>
-						<button onclick="addUser(event)">Add User</button>
-					</div>
+				<h2>User Administration</h2>
+				<div class="form-group">
+				  <label for="user-select">Select User:</label>
+				  <select class="form-control" id="user-select">
+				  	<option></option>
+				    <?php 
+				    	foreach ($data['students'] as $key => $student) {
+				    		$realName = $student->realName;
+				    		echo "<option value='$key'>$realName<option>";
+				    	}
+				     ?>
+				  </select>
 				</div>
+				<div class="well">
+					<div id="user-updated" class="alert alert-success hide">
+					  <a href="#" class="close" data-dismiss="alert" aria-label="close"></a>
+			     	  <div id="update-msg"></div>
+				    </div>
+					<div class="form-group">
+					  <label for="new-name">Change Full Name:</label>
+					  <input type="text" class="form-control" id="new-name">
+					</div>
+					<div class="form-group">
+					  <label for="new-linux">Change Linux Username:</label>
+					  <input type="text" class="form-control" id="new-linux">
+					</div>
+					<div class="well checkbox">
+						<label><input id="update-admin-checkbox" type="checkbox" value="admin">Admin</label>
+					</div>
+					<button style="margin-left: 20px;" onclick="resetPassword(event)">Reset Password</button>
+					<button onclick="deleteUser(event)">Delete User</button>
+					<button onclick="updateUser(event)">Update</button>
+				</div>
+
+				<h2>User Add</h2>
+				<div class="well">
+					<div id="user-added" class="alert alert-success hide">
+					  <a href="#" class="close" data-dismiss="alert" aria-label="close"></a>
+			     	  <div id="msg"></div>
+				    </div>
+					<div class="form-group">
+					  <label for="usr">Name:</label>
+					  <input type="text" class="form-control" id="add-user">
+					</div>
+					<div class="form-group">
+					  <label for="usr">Linux Username:</label>
+					  <input type="text" class="form-control" id="add-linux">
+					</div>
+					<div class="well checkbox">
+							<label><input id="add-admin-checkbox" type="checkbox" value="admin">Admin</label>
+						</div>
+					<button onclick="addUser(event)">Add User</button>
+				</div>
+			</div>
 
 			<div id="project-admin" class="tab-pane fade in panel panel-default">
 				<div class="row panel-body">
@@ -103,5 +104,38 @@
 					</div>
 				</div>
 			</div>
+
+			<div id="team-admin" class="tab-pane fade in panel panel-default">
+				<div class="row panel-body">
+					<div class="col-sm-6">
+					  <h2>Students</h2>
+					  <div class="well droppable" ondrop="drop(event)" ondragover="allowDrop(event)" id="team-select">
+					    <?php 
+					    	foreach ($data['students'] as $student) {
+					    		$name = $student->realName;
+					    		$linux_user = $student->username;
+					    		echo "<div ondragstart='drag(event)' draggable='true' class='student draggable' id='$linux_user'><b>$name</b></div>";
+					    	}
+					    ?>
+					  </div>
+					  <button onclick="addOneStudentPerTeam()" id="one-per-team">One Student Per Team</button>
+					</div>
+					<div class="col-sm-6">
+						<h2>Teams</h2>
+						<div id="team-area" data-numteams="0">
+							<!-- <div class="panel panel-default" id="first:second">
+								<div class="panel-heading">
+									<b>Team 1</b>
+								</div>
+								<div class="panel-body droppable" ondrop="drop(event)" ondragover="allowDrop(event)">
+					            </div>
+					        </div> -->
+					    </div>
+				        <button onclick="makeTeamDiv()">Add Team</button>
+				        <button onclick="saveTeams()">Save Teams</button>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
