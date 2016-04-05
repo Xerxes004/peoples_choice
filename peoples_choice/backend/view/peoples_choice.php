a449173673b5ee122c79905c86a29c94b3e1b5af
Wes Kelly <weskelly@mac.com>
2016-04-05 12:26:44 -0400

    Grades are properly scored.

--- peoples_choice/backend/view/peoples_choice.php
+++ peoples_choice/backend/view/peoples_choice.php
@@ -20,7 +20,20 @@
             $realName = $user->realName;
             echo "<tr><td scope='row' class='test'><a href='http://judah.cedarville.edu/~$username/cs4220.html'>$realName</a></td>";
             foreach ($projs as $proj) {
-              echo "<td class='score first-place'><b>1</b></td>";
+              $first_place_array = $data['grades'][$proj->name]['first'];
+              $second_place = $data['grades'][$proj->name]['second'];
+              $third_place = $data['grades'][$proj->name]['third'];
+              
+              if (in_array($realName, $first_place_array)) {
+                echo "<td class='score first-place'><b>1</b></td>";
+              } else if (in_array($realName, $second_place)) {
+                echo "<td class='score second-place'><b>2</b></td>";
+              } else if (in_array($realName, $third_place)) {
+                echo "<td class='score third-place'><b>3</b></td>";
+              } else {
+                echo '<td></td>';
+              }
+              
             }
             echo '</tr>';
           } 
