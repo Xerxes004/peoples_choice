7aa1ce11f0e0fb1093c753553277eb5c021f31d8
Wes Kelly <weskelly@mac.com>
2016-04-05 00:42:29 -0400

    Changed some style.

--- peoples_choice/backend/view/footer.php
+++ peoples_choice/backend/view/footer.php
@@ -1,4 +1,3 @@
-<footer class="well well-sm" style="margin-top: 20px;color:grey;text-align:center"><em>&copy; Cedarville University <?php date_default_timezone_set('America/Los_Angeles'); echo date("Y"); ?></em></footer>
 </div>
 <div class="col-sm-1"></div>
 </div>
